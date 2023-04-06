7a9bae6308f0a7b18f05777812b9a58cba2ad987
Данияров Алишер Русланович <DaniyarovA@lcoast.itstep>
2023-04-06 21:48:21 +0600

    06.04.23

---
+++ shopMobile-main/src/Screens/screen2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View} from 'react-native';
+import ItemProduct from '../Components/ItemProduct/ItemProduct';
+
+const Screen = ({navigation, route})=>{
+
+  return (
+    <View>
+      <ItemProduct/>
+    </View>
+  )
+}
+export default Screen
